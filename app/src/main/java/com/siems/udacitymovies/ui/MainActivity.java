e2c5d6b2ad7e6fd5ad5926c3666dc332bc1dcef3
Christopher Siems <csiems@gmail.com>
2016-07-29 17:48:43 -0700

    move poster grid to fragment for responsive ui layouts

--- app/src/main/java/com/siems/udacitymovies/ui/MainActivity.java
+++ app/src/main/java/com/siems/udacitymovies/ui/MainActivity.java
@@ -1,36 +1,15 @@
 package com.siems.udacitymovies.ui;
 
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.GridLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.view.Menu;
 import android.view.MenuItem;
 
 import com.siems.udacitymovies.R;
-import com.siems.udacitymovies.adapters.PosterGridAdapter;
-import com.siems.udacitymovies.adapters.SpacesItemDecoration;
-import com.siems.udacitymovies.models.Poster;
-import com.siems.udacitymovies.services.PosterApiService;
-
-import java.io.IOException;
-import java.util.ArrayList;
-
-import butterknife.Bind;
-import butterknife.ButterKnife;
-import okhttp3.Call;
-import okhttp3.Callback;
-import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
-    @Bind(R.id.recyclerView) RecyclerView mRecyclerView;
     public static final String TAG = MainActivity.class.getSimpleName();
-    private GridLayoutManager mGridLayoutManager;
-    private PosterGridAdapter mAdapter;
-    public ArrayList<Poster> mPosters = new ArrayList<>();
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -45,10 +24,6 @@ public class MainActivity extends AppCompatActivity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-        if (id == R.id.action_refresh) {
-            getPopularPosters();
-            return true;
-        }
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             startActivity(new Intent(this, SettingsActivity.class));
@@ -62,45 +37,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ButterKnife.bind(this);
-        getPopularPosters();
     }
 
-    @Override
-    public void onStart() {
-        super.onStart();
-        getPopularPosters();
-    }
 
-    public void getPopularPosters() {
-        final PosterApiService posterApiService = new PosterApiService(this);
-        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(MainActivity.this);
-        String sortOrder = prefs.getString(getString(R.string.pref_sort_key),
-                getString(R.string.pref_sort_default));
 
-        posterApiService.findPosters(sortOrder, new Callback() {
-            @Override
-            public void onFailure(Call call, IOException e) {
-                e.printStackTrace();
-            }
-
-            @Override
-            public void onResponse(Call call, Response response) throws IOException {
-                mPosters = posterApiService.processResults(response);
-
-                MainActivity.this.runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
-                        mAdapter = new PosterGridAdapter(getApplicationContext(), mPosters);
-                        mRecyclerView.setAdapter(mAdapter);
-                        mGridLayoutManager = new GridLayoutManager(MainActivity.this, 2);
-                        mRecyclerView.setLayoutManager(mGridLayoutManager);
-                        mRecyclerView.addItemDecoration(new SpacesItemDecoration(0));
-                        mRecyclerView.setHasFixedSize(true);
-                    }
-                });
-            }
-
-        });
-    }
 }
---
+++ app/src/main/java/com/siems/udacitymovies/ui/PosterGridListFragment.java
@@ -0,0 +1,89 @@
+package com.siems.udacitymovies.ui;
+
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.siems.udacitymovies.R;
+import com.siems.udacitymovies.adapters.PosterGridAdapter;
+import com.siems.udacitymovies.adapters.SpacesItemDecoration;
+import com.siems.udacitymovies.models.Poster;
+import com.siems.udacitymovies.services.PosterApiService;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.Response;
+
+public class PosterGridListFragment extends Fragment {
+    @Bind(R.id.recyclerView) RecyclerView mRecyclerView;
+    private GridLayoutManager mGridLayoutManager;
+    private PosterGridAdapter mAdapter;
+    public ArrayList<Poster> mPosters = new ArrayList<>();
+
+    public PosterGridListFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        getPopularPosters();
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_poster_grid_list, container, false);
+        ButterKnife.bind(this, view);
+        getPopularPosters();
+        return view;
+    }
+
+    public void getPopularPosters() {
+        final PosterApiService posterApiService = new PosterApiService(getActivity());
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getActivity());
+        String sortOrder = prefs.getString(getString(R.string.pref_sort_key),
+                getString(R.string.pref_sort_default));
+
+        posterApiService.findPosters(sortOrder, new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                e.printStackTrace();
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                mPosters = posterApiService.processResults(response);
+
+                getActivity().runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        mAdapter = new PosterGridAdapter(getActivity().getApplicationContext(), mPosters);
+                        mRecyclerView.setAdapter(mAdapter);
+                        mGridLayoutManager = new GridLayoutManager(getActivity(), 2);
+                        mRecyclerView.setLayoutManager(mGridLayoutManager);
+                        mRecyclerView.addItemDecoration(new SpacesItemDecoration(0));
+                        mRecyclerView.setHasFixedSize(true);
+                    }
+                });
+            }
+
+        });
+    }
+
+}
